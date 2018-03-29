fe117b51575446c0e823969f5b9118cb0aaf9641
BADIM-PC\Vadim <grigoruk@gmail.com>
2018-03-30 01:16:41 +0300

    Create menu for build.sh #573 added option for the dependencies

--- build.sh
+++ build.sh
@@ -3,17 +3,17 @@
 ##########################################################################
 # build script for TIC-80
 ##########################################################################
-# Comment the line below if dependencies are already installed.
-sudo apt-get install build-essential mercurial make cmake autoconf automake libtool libasound2-dev libpulse-dev libaudio-dev libx11-dev libxext-dev libxrandr-dev libxcursor-dev libxi-dev libxinerama-dev libxxf86vm-dev libxss-dev libgl1-mesa-dev libesd0-dev libdbus-1-dev libudev-dev libgles1-mesa-dev libgles2-mesa-dev libegl1-mesa-dev libibus-1.0-dev fcitx-libs-dev libsamplerate0-dev libgtk-3-dev zlib1g-dev libwayland-dev libxkbcommon-dev wayland-protocols -y
-echo For a normal build, use the linux target.
-echo -lto targets (Link Time Optimization) have smaller output size.
-echo wasm, emscripten and mingw targets require their compilers to be installed.
+echo "For a normal build, use the linux target."
+echo "-lto targets (Link Time Optimization) have smaller output size."
+echo "wasm, emscripten and mingw targets require their compilers to be installed."
 PS3="Select build target: "
-options=("linux" "linux32-lto" "linux64-lto" "emscripten" "wasm" "mingw" "chip-lto" "exit")
-#make linux
+options=("dependencies" "linux" "linux32-lto" "linux64-lto" "emscripten" "wasm" "mingw" "chip-lto" "exit")
 select opt in "${options[@]}"
 do
   case $opt in
+    "dependencies")
+      sudo apt-get install build-essential mercurial make cmake autoconf automake libtool libasound2-dev libpulse-dev libaudio-dev libx11-dev libxext-dev libxrandr-dev libxcursor-dev libxi-dev libxinerama-dev libxxf86vm-dev libxss-dev libgl1-mesa-dev libesd0-dev libdbus-1-dev libudev-dev libgles1-mesa-dev libgles2-mesa-dev libegl1-mesa-dev libibus-1.0-dev fcitx-libs-dev libsamplerate0-dev libgtk-3-dev zlib1g-dev libwayland-dev libxkbcommon-dev wayland-protocols -y
+      ;;
     "linux")
       make linux
       ;;
