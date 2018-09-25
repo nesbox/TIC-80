3d0d6764a7047765bf897e9c59b950bf87a53704
Vadim Grigoruk <grigoruk@gmail.com>
2018-09-25 11:47:18 +0300

    windows build for sokol

--- examples/sokol/sokol.c
+++ examples/sokol/sokol.c
@@ -1,5 +1,21 @@
 #define SOKOL_IMPL
 
+#if defined(WIN32)
+
+#ifndef WM_MOUSEHWHEEL
+#define WM_MOUSEHWHEEL 0x020E
+#endif
+
+#ifndef AUDCLNT_STREAMFLAGS_AUTOCONVERTPCM
+#define AUDCLNT_STREAMFLAGS_AUTOCONVERTPCM 0x80000000
+#endif
+
+#ifndef AUDCLNT_STREAMFLAGS_SRC_DEFAULT_QUALITY
+#define AUDCLNT_STREAMFLAGS_SRC_DEFAULT_QUALITY 0x08000000
+#endif
+
+#endif
+
 #include "sokol_app.h"
 #include "sokol_gfx.h"
 #include "sokol_time.h"
