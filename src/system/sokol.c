60f423f0a808e29451676c2db1a956d2ddc17121
Vadim Grigoruk <grigoruk@gmail.com>
2018-12-01 17:06:42 +0300

    added licenses

--- src/system/sokol.c
+++ src/system/sokol.c
@@ -1,3 +1,25 @@
+// MIT License
+
+// Copyright (c) 2017 Vadim Grigoruk @nesbox // grigoruk@gmail.com
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -301,9 +323,15 @@ static void handleKeydown(sapp_keycode keycode, bool down)
 
 	};
 
-	if(KeyboardCodes[keycode] > tic_key_unknown)
+	tic_key key = KeyboardCodes[keycode];
+
+	if(key > tic_key_unknown)
 	{
-		platform.keyboard.state[KeyboardCodes[keycode]] = down;
+		// ALT+TAB fix
+		if(key == tic_key_tab && platform.keyboard.state[tic_key_alt])
+			platform.keyboard.state[tic_key_alt] = false;
+
+		platform.keyboard.state[key] = down;
 	}
 }
 
