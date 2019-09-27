9325b5369dca65ea6e7fd9b405b3449abaa67fa9
nesbox <grigoruk@gmail.com>
2019-09-27 19:40:42 +0300

    #898: changed to 2019

--- src/tic.h
+++ src/tic.h
@@ -49,7 +49,7 @@
 #define TIC_NAME_FULL TIC_NAME " tiny computer"
 #define TIC_TITLE TIC_NAME_FULL " " TIC_VERSION_LABEL
 #define TIC_HOST "tic.computer"
-#define TIC_COPYRIGHT "http://" TIC_HOST " (C) 2017"
+#define TIC_COPYRIGHT "http://" TIC_HOST " (C) 2019"
 
 #define TIC_VRAM_SIZE (16*1024) //16K
 #define TIC_RAM_SIZE (TIC_VRAM_SIZE+80*1024) //16K+80K
