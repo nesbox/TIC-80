0f8ed1a9378aa39c46b4b3c51f0f0e9cf4fe60ee
Jeremiasz Nelz <remi6397@gmail.com>
2020-02-13 17:45:39 +0100

    Proper acknowledgement
    Mention of author of significant part of code used in mrbapi.c

--- src/mrbapi.c
+++ src/mrbapi.c
@@ -1,6 +1,7 @@
 ﻿// MIT License
 
-// Copyright (c) Jeremiasz Nelz <remi6397@gmail.com>
+// Copyright (c) 2017 Vadim Grigoruk @nesbox // grigoruk@gmail.com
+// Copyright (c) 2020 Jeremiasz Nelz <remi6397@gmail.com>
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
