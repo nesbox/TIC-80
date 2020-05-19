b3b628c021471ae5bfde0c86327ad6cca60ccd35
Nesbox <grigoruk@gmail.com>
2020-05-19 16:19:12 +0300

    Revert "Enable palette map for sprite transparency" This reverts commit 21c7dd458cb00fc8d3379ed24b2ddb2c34f3a928. Because it breaks compatibility with some carts (https://tic80.com/play?cart=139)

--- src/tic.c
+++ src/tic.c
@@ -302,18 +302,8 @@ static void drawRectBorder(tic_machine* machine, s32 x, s32 y, s32 width, s32 he
 static void drawTile(tic_machine* machine, const tic_tile* buffer, s32 x, s32 y, u8* colors, s32 count, s32 scale, tic_flip flip, tic_rotate rotate)
 {
     static u8 mapping[TIC_PALETTE_SIZE];
-    for (s32 i = 0; i < TIC_PALETTE_SIZE; i++)
-    {
-        u8 mapped = tic_tool_peek4(machine->memory.ram.vram.mapping, i);
-        for (s32 j = 0; j < count; j++)
-        {
-            if (mapped == colors[j]) {
-                mapped = 255;
-                break;
-            }
-        }
-        mapping[i] = mapped;
-    }
+    for (s32 i = 0; i < TIC_PALETTE_SIZE; i++) mapping[i] = tic_tool_peek4(machine->memory.ram.vram.mapping, i);
+    for (s32 i = 0; i < count; i++) mapping[colors[i]] = 255;
 
     rotate &= 0b11;
     u32 orientation = flip & 0b11;
