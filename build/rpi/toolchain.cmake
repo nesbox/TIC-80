73d3be795a88a9253cdde5a9ebc56090f7fa858c
nesbox <grigoruk@gmail.com>
2020-10-21 21:37:41 +0300

    added curl to rpi build

--- build/rpi/toolchain.cmake
+++ build/rpi/toolchain.cmake
@@ -1,23 +1,30 @@
 # Define our host system
-SET(CMAKE_SYSTEM_NAME Linux)
-SET(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
 
-SET(CMAKE_LIBRARY_ARCHITECTURE arm-linux-gnueabihf)
-set(RPI_HOME $ENV{RPI_HOME})
+set(CMAKE_LIBRARY_ARCHITECTURE arm-linux-gnueabihf)
+
+if(NOT DEFINED RPI_HOME)
+	set(RPI_HOME $ENV{RPI_HOME})
+endif()
+
 set(SYSROOT_PATH ${RPI_HOME}/sysroot)
 
 # Define the cross compiler locations
-SET(CMAKE_C_COMPILER   ${RPI_HOME}/tools/arm-bcm2708/${CMAKE_LIBRARY_ARCHITECTURE}/bin/${CMAKE_LIBRARY_ARCHITECTURE}-gcc)
-SET(CMAKE_CXX_COMPILER ${RPI_HOME}/tools/arm-bcm2708/${CMAKE_LIBRARY_ARCHITECTURE}/bin/${CMAKE_LIBRARY_ARCHITECTURE}-g++)
+set(CMAKE_C_COMPILER   ${RPI_HOME}/tools/arm-bcm2708/${CMAKE_LIBRARY_ARCHITECTURE}/bin/${CMAKE_LIBRARY_ARCHITECTURE}-gcc)
+set(CMAKE_CXX_COMPILER ${RPI_HOME}/tools/arm-bcm2708/${CMAKE_LIBRARY_ARCHITECTURE}/bin/${CMAKE_LIBRARY_ARCHITECTURE}-g++)
 
 # Define the sysroot path for the RaspberryPi distribution in our tools folder 
-SET(CMAKE_FIND_ROOT_PATH ${SYSROOT_PATH})
+set(CMAKE_FIND_ROOT_PATH ${SYSROOT_PATH})
 
 # Use our definitions for compiler tools
-SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
 # Search for libraries and headers in the target directories only
 set( CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY )
 set( CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY )
+
+include_directories(${SYSROOT_PATH}/usr/include)
+include_directories(${SYSROOT_PATH}/usr/include/${CMAKE_LIBRARY_ARCHITECTURE})
 
 set(RPI TRUE)
