f7959128d8033e83d3d58c72cbba97c70f9eba5c
Nesbox <grigoruk@gmail.com>
2020-12-04 13:06:53 +0300

    #1222: build rpi and baremetalpi using docker

--- build/rpi/toolchain.cmake
+++ build/rpi/toolchain.cmake
@@ -3,16 +3,11 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_VERSION 1)
 
 set(CMAKE_LIBRARY_ARCHITECTURE arm-linux-gnueabihf)
-
-if(NOT DEFINED RPI_HOME)
-	set(RPI_HOME $ENV{RPI_HOME})
-endif()
-
-set(SYSROOT_PATH ${RPI_HOME}/sysroot)
+set(SYSROOT_PATH /opt/sysroot)
 
 # Define the cross compiler locations
-set(CMAKE_C_COMPILER   ${RPI_HOME}/tools/arm-bcm2708/${CMAKE_LIBRARY_ARCHITECTURE}/bin/${CMAKE_LIBRARY_ARCHITECTURE}-gcc)
-set(CMAKE_CXX_COMPILER ${RPI_HOME}/tools/arm-bcm2708/${CMAKE_LIBRARY_ARCHITECTURE}/bin/${CMAKE_LIBRARY_ARCHITECTURE}-g++)
+set(CMAKE_C_COMPILER   /opt/tools/arm-bcm2708/${CMAKE_LIBRARY_ARCHITECTURE}/bin/${CMAKE_LIBRARY_ARCHITECTURE}-gcc)
+set(CMAKE_CXX_COMPILER /opt/tools/arm-bcm2708/${CMAKE_LIBRARY_ARCHITECTURE}/bin/${CMAKE_LIBRARY_ARCHITECTURE}-g++)
 
 # Define the sysroot path for the RaspberryPi distribution in our tools folder 
 set(CMAKE_FIND_ROOT_PATH ${SYSROOT_PATH})
