9b3db7bf72ddd44cf61f5b566f346ab6edb862a6
Joshua Minor <github@pixelverse.org>
2021-04-18 14:28:08 -0700

    Stubs for all API functions

--- src/api/micropython.c
+++ src/api/micropython.c
@@ -45,33 +45,14 @@ void nlr_jump_fail(void *val) {
     exit(1);
 }
 
-// A simple function that adds its 2 arguments (must be integers)
-STATIC mp_obj_t add(mp_obj_t x_in, mp_obj_t y_in) {
-    mp_int_t x = mp_obj_get_int(x_in);
-    mp_int_t y = mp_obj_get_int(y_in);
-    int r = x+y;
-    fprintf(stderr, "add(%ld,%ld) -> %d\n", x, y, r);
-    return mp_obj_new_int(r);
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_2(add_obj, add);
-
-STATIC mp_obj_t python_btn(size_t n_args, const mp_obj_t *args) {
-    if (n_args == 0) {
-        return mp_obj_new_int(python_vm.mem->ram.input.gamepads.data);
-    }else{
-        mp_int_t index = mp_obj_get_int(args[0]) & 0x1f;
-        u32 r = python_vm.mem->ram.input.gamepads.data & (1 << index);
-        return mp_obj_new_bool(r);
-    }
-}
-STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(btn_obj, 0, 1, python_btn);
-
+// cls [color=0]
 STATIC mp_obj_t python_cls(size_t n_args, const mp_obj_t *args) {
     tic_api_cls(python_vm.mem, n_args == 1 ? mp_obj_get_int(args[0]) : 0);
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(cls_obj, 0, 1, python_cls);
 
+// spr id x y [colorkey=-1] [scale=1] [flip=0] [rotate=0] [w=1 h=1]
 STATIC mp_obj_t python_spr(size_t n_args, const mp_obj_t *args) {
     s32 index = 0;
     s32 x = 0;
@@ -145,6 +126,242 @@ STATIC mp_obj_t python_spr(size_t n_args, const mp_obj_t *args) {
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(spr_obj, 1, 9, python_spr);
 
+// print text [x=0 y=0] [color=12] [fixed=false] [scale=1] [smallfont=false] -> text width
+STATIC mp_obj_t python_print(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(print_obj, 1, 7, python_print);
+
+// pix x y color
+// pix x y -> color
+STATIC mp_obj_t python_pix(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(pix_obj, 2, 3, python_pix);
+
+// line x0 y0 x1 y1 color
+STATIC mp_obj_t python_line(size_t n_args, const mp_obj_t *args) {
+    mp_obj_t x0_in = args[0];
+    mp_obj_t y0_in = args[1];
+    mp_obj_t x1_in = args[2];
+    mp_obj_t y1_in = args[3];
+    mp_obj_t color_in = args[4];
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(line_obj, 5, 5, python_line);
+
+// rect x y w h color
+STATIC mp_obj_t python_rect(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(rect_obj, 5, 5, python_rect);
+
+// rectb x y w h color
+STATIC mp_obj_t python_rectb(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(rectb_obj, 5, 5, python_rectb);
+
+// btn [id] -> pressed
+STATIC mp_obj_t python_btn(size_t n_args, const mp_obj_t *args) {
+    if (n_args == 0) {
+        return mp_obj_new_int(python_vm.mem->ram.input.gamepads.data);
+    }else{
+        mp_int_t index = mp_obj_get_int(args[0]) & 0x1f;
+        u32 r = python_vm.mem->ram.input.gamepads.data & (1 << index);
+        return mp_obj_new_bool(r);
+    }
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(btn_obj, 0, 1, python_btn);
+
+// btnp [[id, [hold], [period] ] -> pressed
+STATIC mp_obj_t python_btnp(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(btnp_obj, 0, 3, python_btnp);
+
+// sfx id [note] [duration=-1] [channel=0] [volume=15] [speed=0]
+STATIC mp_obj_t python_sfx(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(sfx_obj, 1, 6, python_sfx);
+
+// map [x=0 y=0] [w=30 h=17] [sx=0 sy=0] [colorkey=-1] [scale=1] [remap=nil]
+STATIC mp_obj_t python_map(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(map_obj, 0, 9, python_map);
+
+// mget x y -> tile_id
+STATIC mp_obj_t python_mget(mp_obj_t x_in, mp_obj_t y_in) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(mget_obj, python_mget);
+
+// mset x y tile_id
+STATIC mp_obj_t python_mset(mp_obj_t x_in, mp_obj_t y_in, mp_obj_t tile_id_in) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(mset_obj, python_mset);
+
+// peek addr
+STATIC mp_obj_t python_peek(mp_obj_t addr_in) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(peek_obj, python_peek);
+
+// poke addr val
+STATIC mp_obj_t python_poke(mp_obj_t addr_in, mp_obj_t val_in) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(poke_obj, python_poke);
+
+// peek4 addr4
+STATIC mp_obj_t python_peek4(mp_obj_t addr4_in) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(peek4_obj, python_peek4);
+
+// poke4 addr4 val
+STATIC mp_obj_t python_poke4(mp_obj_t addr4_in, mp_obj_t val_in) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(poke4_obj, python_poke4);
+
+// memcpy to from length
+STATIC mp_obj_t python_memcpy(mp_obj_t to_in, mp_obj_t from_in, mp_obj_t length_in) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(memcpy_obj, python_memcpy);
+
+// memset addr value length
+STATIC mp_obj_t python_memset(mp_obj_t addr_in, mp_obj_t value_in, mp_obj_t length_in) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(memset_obj, python_memset);
+
+// trace message [color]
+STATIC mp_obj_t python_trace(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(trace_obj, 1, 2, python_trace);
+
+// pmem index [val] -> val
+STATIC mp_obj_t python_pmem(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(pmem_obj, 1, 2, python_pmem);
+
+// time
+STATIC mp_obj_t python_time() {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(time_obj, python_time);
+
+// tstamp
+STATIC mp_obj_t python_tstamp() {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(tstamp_obj, python_tstamp);
+
+// exit
+STATIC mp_obj_t python_exit() {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(exit_obj, python_exit);
+
+// font text, x, y, [transcolor], [char width], [char height], [fixed=false], [scale=1] -> text width
+STATIC mp_obj_t python_font(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(font_obj, 3, 8, python_font);
+
+// mouse
+STATIC mp_obj_t python_mouse() {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(mouse_obj, python_mouse);
+
+// circ x y radius color
+STATIC mp_obj_t python_circ(size_t n_args, const mp_obj_t *args) {
+    mp_obj_t x_in = args[0];
+    mp_obj_t y_in = args[1];
+    mp_obj_t radius_in = args[2];
+    mp_obj_t color_in = args[3];
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(circ_obj, 4, 4, python_circ);
+
+// circb x y radius color
+STATIC mp_obj_t python_circb(size_t n_args, const mp_obj_t *args) {
+    mp_obj_t x_in = args[0];
+    mp_obj_t y_in = args[1];
+    mp_obj_t radius_in = args[2];
+    mp_obj_t color_in = args[3];
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(circb_obj, 4, 4, python_circb);
+
+// tri x1 y1 x2 y2 x3 y3 color
+STATIC mp_obj_t python_tri(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(tri_obj, 7, 7, python_tri);
+
+// textri x1 y1 x2 y2 x3 y3 u1 v1 u2 v2 u3 v3 [use_map=false] [trans=-1]
+STATIC mp_obj_t python_textri(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(textri_obj, 12, 14, python_textri);
+
+// clip [x y width height]
+STATIC mp_obj_t python_clip(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(clip_obj, 0, 4, python_clip);
+
+// music [track=-1] [frame=-1] [row=-1] [loop=true] [sustain=false]
+STATIC mp_obj_t python_music(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(music_obj, 0, 5, python_music);
+
+// sync [mask=0] [bank=0] [tocart=false]
+STATIC mp_obj_t python_sync(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(sync_obj, 0, 3, python_sync);
+
+// reset
+STATIC mp_obj_t python_reset() {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(reset_obj, python_reset);
+
+// key [code] -> pressed
+STATIC mp_obj_t python_key(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(key_obj, 0, 1, python_key);
+
+// keyp [code [hold period] ] -> pressed
+STATIC mp_obj_t python_keyp(size_t n_args, const mp_obj_t *args) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(keyp_obj, 0, 2, python_keyp);
+
+// fget sprite_id flag -> bool
+STATIC mp_obj_t python_fget(mp_obj_t sprite_id_in, mp_obj_t flag_in) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_2(fget_obj, python_fget);
+
+// fset sprite_id flag bool
+STATIC mp_obj_t python_fset(mp_obj_t sprite_id_in, mp_obj_t flag_in, mp_obj_t bool_in) {
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(fset_obj, python_fset);
+
 static bool initPython(tic_mem* tic, const char* code)
 {
     mp_stack_set_limit(40000 * (BYTES_PER_WORD / 4));
@@ -199,10 +416,16 @@ static bool initPython(tic_mem* tic, const char* code)
             // mp_obj_print_exception(&mp_plat_print, ((mp_obj_t)nlr.ret_val));
         }
 
-        mp_store_global(qstr_from_str("add"), MP_OBJ_FROM_PTR(&add_obj));
-        mp_store_global(qstr_from_str("btn"), MP_OBJ_FROM_PTR(&btn_obj));
-        mp_store_global(qstr_from_str("cls"), MP_OBJ_FROM_PTR(&cls_obj));
-        mp_store_global(qstr_from_str("spr"), MP_OBJ_FROM_PTR(&spr_obj));
+        // Instead of manually listing each of these...
+        // mp_store_global(qstr_from_str("btn"), MP_OBJ_FROM_PTR(&btn_obj));
+        // mp_store_global(qstr_from_str("cls"), MP_OBJ_FROM_PTR(&cls_obj));
+        // mp_store_global(qstr_from_str("spr"), MP_OBJ_FROM_PTR(&spr_obj));
+        // We use the TIC_API_LIST macro to generate them all.
+        // This also helps to remind us to implement a new wrapper if/when
+        // some new API is added.
+#define API_FUNC_DEF(name, ...) {mp_store_global(qstr_from_str(#name), MP_OBJ_FROM_PTR(&name ## _obj));}
+    TIC_API_LIST(API_FUNC_DEF)
+#undef API_FUNC_DEF
 
         // qstr xstr = qstr_from_str("x");
         // fprintf(stderr, "xstr = %d\n", (int)xstr);
