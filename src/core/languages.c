c94e3188776fe4586e04cb4511832263de04b13e
msx80 <msx80@github>
2021-09-08 16:51:29 +0200

    switches for languages

--- src/core/languages.c
+++ src/core/languages.c
@@ -1,20 +1,55 @@
 #include <stddef.h>
 #include "api.h"
 
+#if defined(TIC_BUILD_WITH_JS)
 extern tic_script_config JsSyntaxConfig;
+#endif
+
+#if defined (TIC_BUILD_WITH_LUA)
 extern tic_script_config LuaSyntaxConfig;
+#endif
+
+#if defined(TIC_BUILD_WITH_MOON)
 extern tic_script_config MoonSyntaxConfig;
+#endif
+
+#if defined(TIC_BUILD_WITH_FENNEL)
 extern tic_script_config FennelSyntaxConfig;
+#endif
+
+#if defined(TIC_BUILD_WITH_SQUIRREL)
 extern tic_script_config SquirrelSyntaxConfig;
+#endif
+
+#if defined(TIC_BUILD_WITH_WREN)
 extern tic_script_config WrenSyntaxConfig;
+#endif
 
 tic_script_config* Languages[] = {
-&LuaSyntaxConfig, 
-&JsSyntaxConfig,
-&MoonSyntaxConfig, 
-&FennelSyntaxConfig, 
-&SquirrelSyntaxConfig,
-&WrenSyntaxConfig,
-NULL};
 
+	#if defined (TIC_BUILD_WITH_LUA)
+	&LuaSyntaxConfig,
+	#endif
+
+	#if defined(TIC_BUILD_WITH_JS)
+	&JsSyntaxConfig,
+	#endif
+
+	#if defined(TIC_BUILD_WITH_MOON)
+	&MoonSyntaxConfig,
+	#endif
+
+	#if defined(TIC_BUILD_WITH_FENNEL)
+	&FennelSyntaxConfig,
+	#endif
+
+	#if defined(TIC_BUILD_WITH_SQUIRREL)
+	&SquirrelSyntaxConfig,
+	#endif
+
+	#if defined(TIC_BUILD_WITH_WREN)
+	&WrenSyntaxConfig,
+	#endif
+
+	NULL};
 
