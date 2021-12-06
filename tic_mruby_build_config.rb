415a6f31c16d328e36a711d943b1cadb06077183
Jeremiasz Nelz <jeremiasz@nelz.pl>
2021-12-07 00:40:49 +0100

    Build mruby with /MT on Windows

--- tic_mruby_build_config.rb
+++ tic_mruby_build_config.rb
@@ -19,6 +19,8 @@ MRuby::Build.new do |conf|
   # C compiler settings
   conf.cc do |cc|
     cc.flags << '-fPIC' unless ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+    cc.flags.delete '/MD' if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+    cc.flags << '/MT' if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
   #   cc.command = ENV['CC'] || 'gcc'
   #   cc.flags = [ENV['CFLAGS'] || %w()]
   #   cc.include_paths = ["#{root}/include"]
