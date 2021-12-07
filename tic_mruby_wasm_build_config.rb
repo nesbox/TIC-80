a883fc91f5134df897b0dcb388663d79cef2e8e7
Jeremiasz Nelz <jeremiasz@nelz.pl>
2021-12-07 17:55:17 +0100

    Revert "Pass CMake flags to MRuby cross target"
    This reverts commit 1a8bf8330ecfcfe83082476282156a867796d8a8.
    
    It turns out that wrapping CMake's poor cross build support
    was not a good idea after all.

---
+++ tic_mruby_wasm_build_config.rb
@@ -0,0 +1,22 @@
+MRuby::Toolchain.new('emscripten') do |conf|
+  toolchain :clang
+  
+  conf.cc.command = 'emcc'
+  conf.cxx.command = 'emcc'
+  conf.linker.command = 'emcc'
+  conf.archiver.command = 'emar'
+end
+
+MRuby::CrossBuild.new('wasm') do |conf|
+  toolchain :emscripten
+
+  # include the GEM box
+  conf.gembox File.expand_path('tic', File.dirname(__FILE__))
+
+  conf.cc do |cc|
+    cc.flags = [ENV['CFLAGS'] || %w()]
+  end
+  
+  conf.enable_bintest
+  conf.enable_test
+end
