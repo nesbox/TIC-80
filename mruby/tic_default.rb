d8c913f3bd8b691439afaa7a0a82ac0c1c10572e
Jeremiasz Nelz <jeremiasz@nelz.pl>
2021-12-08 17:19:28 +0100

    Simplify mruby build system
    * Genericize cross compilation
    * Clean mruby build artifacts with make/ninja clean

--- mruby/tic_default.rb
+++ mruby/tic_default.rb
@@ -1,57 +1,40 @@
 MRuby::Build.new do |conf|
-  # load specific toolchain settings
   conf.toolchain
 
-  # include the GEM box
   conf.gembox 'default'
 
   conf.enable_bintest
   conf.enable_test
 end
 
-MRuby::Toolchain.new('emscripten') do |conf|
-  toolchain :clang
+MRuby::CrossBuild.new('target') do |conf|
+  toolchain ENV['MRUBY_TOOLCHAIN']
 
-  conf.cc.command = ENV['TARGET_CC'] || 'emcc'
-  conf.cxx.command = ENV['TARGET_CXX'] || 'emcc'
-  conf.linker.command = ENV['TARGET_LD'] || 'emcc'
-  conf.archiver.command = ENV['TARGET_AR'] || 'emar'
-end
-
-BuildClazz = (ENV['MRUBY_CROSS_TOOLCHAIN'] || '').empty? ? MRuby::Build : MRuby::CrossBuild
-
-BuildClazz.new('target') do |conf|
-  # load specific toolchain settings
-  if (ENV['MRUBY_TOOLCHAIN'] || '').empty?
-    conf.toolchain
-  else
-    toolchain ENV['MRUBY_TOOLCHAIN']
-  end
-
-  # include the GEM box
   conf.gembox File.expand_path('tic', File.dirname(__FILE__))
 
-  # C compiler settings
   conf.cc do |cc|
+    cc.command = ENV["TARGET_CC"] || 'cc'
     cc.flags = [ENV["TARGET_CFLAGS"] || %w()]
     unless ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
       cc.flags << '-fPIC' 
     end
   end
 
-  # Linker settings
   conf.linker do |linker|
-    linker.flags = [ENV['TARGET_LDFLAGS'] || []]
+    linker.command = ENV['TARGET_LD'] || 'ld'
+    linker.flags = [ENV['TARGET_LDFLAGS'] || %w()]
   end
 
-  # file extensions
+  conf.archiver do |archiver|
+    archiver.command = ENV['TARGET_AR'] || 'ar'
+  end
+
   conf.exts do |exts|
     exts.object = '.o'
     exts.library = '.a'
     # exts.executable = '' # '.exe' if Windows
   end
 
-  # Turn on `enable_debug` for better debugging
   conf.enable_debug if /DEBUG/ =~ ENV["BUILD_TYPE"]
   conf.enable_bintest
   conf.enable_test
