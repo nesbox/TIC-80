64862da6ef0845d5523104ba22a3dca694d3f36d
Tyler887 <tylermageeshields@gmail.com>
2021-12-19 19:16:04 +0000

    Create hello.c

---
+++ demos/hello.c
@@ -0,0 +1,13 @@
+-- title: Hello World
+-- author: Nesbox
+-- desc: Your first TIC-80 game
+-- script: c
+
+#include <iostream>
+using namespace std;
+​
+int main() {
+  cout << "Hello World!";
+  return 0;
+}
+​
