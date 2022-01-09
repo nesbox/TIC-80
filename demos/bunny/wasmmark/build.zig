b59c8b75c93d117745cd3fe333010d5eb4fe592b
Josh Goebel <me@joshgoebel.com>
2022-01-09 13:38:32 -0500

    update build.zig and keep in sync

--- demos/bunny/wasmmark/build.zig
+++ demos/bunny/wasmmark/build.zig
@@ -7,14 +7,13 @@ pub fn build(b: *std.build.Builder) void {
     lib.setBuildMode(mode);
     lib.setTarget(.{ .cpu_arch = .wasm32, .os_tag = .freestanding });
     lib.import_memory = true;
-    lib.initial_memory = 64 * 1024 * 4;
-    lib.max_memory = 64 * 1024 * 4;
-    // 0x14e04
-    // + 1024 is because we keep adding stuff, we really shouldn't do that
+    lib.initial_memory = 65536 * 4;
+    lib.max_memory = 65536 * 4;
     lib.export_table = true;
-    // lib.global_base = 85508 + 1024;
+    // all the memory below 96kb is reserved for TIC and memory mapped I/O
+    // so our own usage must start above the 96kb mark
     lib.global_base = 96 * 1024;
     lib.stack_size = 8192;
-    lib.export_symbol_names = &[_][]const u8{ "TIC", "OVR", "BDR", "SCR" };
+    lib.export_symbol_names = &[_][]const u8{ "TIC", "OVR", "BDR", "SCR", "INIT" };
     lib.install();
 }
--- templates/zig/build.zig
+++ templates/zig/build.zig
@@ -7,13 +7,13 @@ pub fn build(b: *std.build.Builder) void {
     lib.setBuildMode(mode);
     lib.setTarget(.{ .cpu_arch = .wasm32, .os_tag = .freestanding });
     lib.import_memory = true;
-    lib.initial_memory = 65536 * 2;
-    lib.max_memory = 65536 * 2;
+    lib.initial_memory = 65536 * 4;
+    lib.max_memory = 65536 * 4;
     lib.export_table = true;
     // all the memory below 96kb is reserved for TIC and memory mapped I/O
     // so our own usage must start above the 96kb mark
     lib.global_base = 96 * 1024;
     lib.stack_size = 8192;
-    lib.export_symbol_names = &[_][]const u8{ "TIC", "OVR", "BDR", "SCR", "BOOT" };
+    lib.export_symbol_names = &[_][]const u8{ "TIC", "OVR", "BDR", "SCR", "INIT" };
     lib.install();
 }
