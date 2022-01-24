7d97e6b713da1f01bf90ef7e9dc4b0918e8bffb4
Rob Loach <robloach@gmail.com>
2022-01-23 20:59:02 -0500

    mruby: Only build the Ruby demos if building with mruby

--- src/studio/demos.c
+++ src/studio/demos.c
@@ -187,7 +187,7 @@ tic_script_config_extra* LanguagesExtra[] = {
 #if defined(TIC_BUILD_WITH_JS)
    &JsSyntaxConfigExtra,
 #endif
-#if defined(TIC_BUILD_WITH_MRUBY)
+#if defined(TIC_BUILD_WITH_MRUBY) && TIC_BUILD_WITH_MRUBY
    &MRubySyntaxConfigExtra,
 #endif
    NULL
