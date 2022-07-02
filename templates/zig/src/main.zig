3d1f1a7206462c1c5d151e74b965bfe97a0ef184
Finn Coffey <finn.coffey@gmail.com>
2022-07-02 10:40:26 +1000

    Run zig fmt on demo code to ensure consistency

--- templates/zig/src/main.zig
+++ templates/zig/src/main.zig
@@ -1,34 +1,32 @@
 const tic = @import("tic80.zig");
 
 const TICGuy = struct {
-    x : i32 = 96,
-    y : i32 = 24,
+    x: i32 = 96,
+    y: i32 = 24,
 };
 
-var t : u16 = 0;
-var mascot : TICGuy = .{};
+var t: u16 = 0;
+var mascot: TICGuy = .{};
 
-export fn BOOT() void {
-}
+export fn BOOT() void {}
 
 export fn TIC() void {
     if (tic.btn(0)) {
         mascot.y -= 1;
-    } 
+    }
     if (tic.btn(1)) {
-        mascot.y +=1;
-    } 
+        mascot.y += 1;
+    }
     if (tic.btn(2)) {
         mascot.x -= 1;
-    } 
+    }
     if (tic.btn(3)) {
         mascot.x += 1;
-    } 
+    }
 
     tic.cls(13);
-    tic.spr(@as(i32, 1+t%60/30*2),mascot.x,mascot.y,.{.w=2, .h=2, .transparent=&.{14}, .scale=3});
+    tic.spr(@as(i32, 1 + t % 60 / 30 * 2), mascot.x, mascot.y, .{ .w = 2, .h = 2, .transparent = &.{14}, .scale = 3 });
     _ = tic.print("HELLO WORLD!", 84, 84, .{});
-    
+
     t += 1;
 }
-
