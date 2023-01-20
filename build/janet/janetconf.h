4d2b4a07d1e8fe6b5a2b9809f01ed2746f700f8b
alectroemel <alectroemel@hotmail.com>
2023-01-20 09:44:23 -0600

    include file extension for janet build windows, make single threaded

--- build/janet/janetconf.h
+++ build/janet/janetconf.h
@@ -12,7 +12,7 @@
 /* #define JANET_BUILD "local" */
 
 /* These settings all affect linking, so use cautiously. */
-/* #define JANET_SINGLE_THREADED */
+#define JANET_SINGLE_THREADED
 #define JANET_NO_DYNAMIC_MODULES
 #define JANET_NO_NANBOX
 /* #define JANET_API __attribute__((visibility ("default"))) */
@@ -28,6 +28,7 @@
 /* #define JANET_NO_PEG */
 #define JANET_NO_NET
 #define JANET_NO_FFI
+#define JANET_NO_FFI_JIT
 /* #define JANET_NO_INT_TYPES */
 #define JANET_NO_EV
 /* #define JANET_NO_REALPATH */
