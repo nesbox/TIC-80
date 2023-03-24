5354809d34f3caa5de1d2cf59a9c1dea80eb6792
alectroemel <alec@mirusresearch.com>
2023-03-24 09:23:48 -0500

    update janet conf version

--- build/janet/janetconf.h
+++ build/janet/janetconf.h
@@ -4,10 +4,10 @@
 #define JANETCONF_H
 
 #define JANET_VERSION_MAJOR 1
-#define JANET_VERSION_MINOR 26
+#define JANET_VERSION_MINOR 27
 #define JANET_VERSION_PATCH 1
-#define JANET_VERSION_EXTRA "-dev"
-#define JANET_VERSION "1.26.1-dev"
+#define JANET_VERSION_EXTRA ""
+#define JANET_VERSION "1.27.0"
 
 /* #define JANET_BUILD "local" */
 
