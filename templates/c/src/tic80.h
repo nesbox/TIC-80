54672c4534c61aad4cf19300584b2a911e9376d7
Gota7 <gotaseven@gmail.com>
2023-07-02 23:08:09 -0400

    C Fixes Done

--- templates/c/src/tic80.h
+++ templates/c/src/tic80.h
@@ -111,10 +111,6 @@ enum BUTTON_CODES
     BUTTON_CODE_P4_Y
 };
 
-#define TILE_SIZE 8
-#define WIDTH 240
-#define HEIGHT 136
-
 typedef struct {
     uint8_t SCREEN[16320];
     uint8_t PALETTE[48];
@@ -126,6 +122,16 @@ typedef struct {
     uint8_t BLIT_SEGMENT;
     uint8_t RESERVED[3];
 } VRAM;
+
+typedef struct {
+    int16_t x; int16_t  y;
+    int8_t scrollx; int8_t scrolly;
+    bool left; bool middle; bool right;
+} MouseData;
+
+#define TILE_SIZE 8
+#define WIDTH 240
+#define HEIGHT 136
 
 // Pointers.
 VRAM* FRAMEBUFFER = (VRAM*)0;
@@ -168,12 +174,6 @@ const uint32_t SPRITE_FLAGS_SIZE = 512;
 const uint32_t SYSTEM_FONT_SIZE = 2048;
 const uint32_t WASM_FREE_RAM_SIZE = 163840; // 160kb
 
-typedef struct {
-    int16_t x; int16_t  y;
-    int8_t scrollx; int8_t scrolly;
-    bool left; bool middle; bool right;
-} MouseData;
-
 WASM_IMPORT("btn")
 int32_t btn(int32_t index);
 
@@ -207,98 +207,98 @@ bool fget(int32_t sprite_index, int8_t flag);
 WASM_IMPORT("fset")
 bool fset(int32_t sprite_index, int8_t flag, bool value);
 
-WASM_IMPORT("font");
+WASM_IMPORT("font")
 int8_t font(const uint8_t* text, int32_t x, int32_t y, const uint8_t* trans_colors, int8_t trans_count, int8_t char_width, int8_t char_height, bool fixed, int32_t scale, bool alt);
 
-WASM_IMPORT("key");
+WASM_IMPORT("key")
 bool key(int32_t x);
 
-WASM_IMPORT("keyp");
+WASM_IMPORT("keyp")
 bool keyp(int32_t x, int32_t hold, int32_t period);
 
-WASM_IMPORT("line");
+WASM_IMPORT("line")
 void line(float x0, float y0, float x1, float y1, uint8_t color);
 
-WASM_IMPORT("map");
+WASM_IMPORT("map")
 void map(int32_t x, int32_t y, int32_t w, int32_t h, int32_t sx, int32_t sy, const uint8_t* trans_colors, int8_t color_count, int8_t scale, int32_t remap);
 
-WASM_IMPORT("mget");
+WASM_IMPORT("mget")
 int32_t mget(int32_t x, int32_t y);
 
-WASM_IMPORT("mset");
+WASM_IMPORT("mset")
 void mset(int32_t x, int32_t y, int32_t value);
 
 WASM_IMPORT("mouse")
 void mouse(MouseData* data);
 
-WASM_IMPORT("music");
+WASM_IMPORT("music")
 void music(int32_t track, int32_t frame, int32_t w, bool repeat, bool sustain, int32_t tempo, int32_t speed);
 
-WASM_IMPORT("pix");
+WASM_IMPORT("pix")
 uint8_t pix(int32_t x, int32_t y, int8_t color);
 
-WASM_IMPORT("peek");
+WASM_IMPORT("peek")
 uint8_t peek(int32_t address,  uint8_t bits);
 
-WASM_IMPORT("peek4");
+WASM_IMPORT("peek4")
 uint8_t peek4(int32_t address);
 
-WASM_IMPORT("peek2");
+WASM_IMPORT("peek2")
 uint8_t peek2(int32_t address);
 
-WASM_IMPORT("peek1");
+WASM_IMPORT("peek1")
 uint8_t peek1(int32_t address);
 
-WASM_IMPORT("pmem");
+WASM_IMPORT("pmem")
 int32_t pmem(int32_t address, int64_t value);
 
-WASM_IMPORT("poke");
+WASM_IMPORT("poke")
 void poke(int32_t address, uint8_t value, uint8_t bits);
 
-WASM_IMPORT("poke4");
+WASM_IMPORT("poke4")
 void poke4(int32_t address, uint8_t value);
 
-WASM_IMPORT("poke2");
+WASM_IMPORT("poke2")
 void poke2(int32_t address, uint8_t value);
 
-WASM_IMPORT("poke1");
+WASM_IMPORT("poke1")
 void poke1(int32_t address, uint8_t value);
 
-WASM_IMPORT("print");
+WASM_IMPORT("print")
 int32_t print(const char* text, int32_t x, int32_t y, int32_t color, bool fixed, int32_t scale, bool alt);
 
-WASM_IMPORT("rect");
+WASM_IMPORT("rect")
 void rect(int32_t x, int32_t y, int32_t w, int32_t h, int32_t color);
 
-WASM_IMPORT("rectb");
+WASM_IMPORT("rectb")
 void rectb(int32_t x, int32_t y, int32_t w, int32_t h, int32_t color);
 
-WASM_IMPORT("sfx");
+WASM_IMPORT("sfx")
 void sfx(int32_t sfx_id, int32_t note, int32_t octave, int32_t duration, int32_t channel, int32_t volume_left, int32_t volume_right, int32_t speed);
 
-WASM_IMPORT("spr");
+WASM_IMPORT("spr")
 void spr(int32_t id, int32_t x, int32_t y, const uint8_t* trans_colors, int8_t color_count, int32_t scale, int32_t flip, int32_t rotate, int32_t w, int32_t h);
 
-WASM_IMPORT("sync");
+WASM_IMPORT("sync")
 void sync(int32_t mask, uint8_t bank, bool to_cart);
 
-WASM_IMPORT("time");
+WASM_IMPORT("time")
 float time();
 
-WASM_IMPORT("tstamp");
+WASM_IMPORT("tstamp")
 uint32_t tstamp();
 
-WASM_IMPORT("trace");
+WASM_IMPORT("trace")
 void trace(const uint8_t* text, uint8_t color);
 
-WASM_IMPORT("tri");
+WASM_IMPORT("tri")
 void tri(float x1, float y1, float x2, float y2, float x3, float y3, uint8_t color);
 
-WASM_IMPORT("trib");
+WASM_IMPORT("trib")
 void trib(float x1, float y1, float x2, float y2, float x3, float y3, uint8_t color);
 
-WASM_IMPORT("ttri");
+WASM_IMPORT("ttri")
 void ttri( float x1, float y1, float x2, float y2, float x3, float y3, float u1, float v1, float u2, float v2, float u3, float v3, int32_t tex_src, const uint8_t* trans_colors, int8_t color_count, float z1, float z2, float z3, bool depth);
 
-WASM_IMPORT("vbank");
+WASM_IMPORT("vbank")
 uint8_t vbank(uint8_t bank);
