5b9739967a4fd07a035f72b45133d400a93eb3f5
nesbox <grigoruk@gmail.com>
2023-08-24 17:19:28 +0200

    #2229: fixed CRT effect in alone exports

--- src/studio/config.c
+++ src/studio/config.c
@@ -88,56 +88,6 @@ static void readGlobalBool(lua_State* lua, const char* name, bool* val)
     lua_pop(lua, 1);
 }
 
-#if defined(CRT_SHADER_SUPPORT)
-
-static void readString(lua_State* lua, const char* name, const char** val)
-{
-    lua_getfield(lua, -1, name);
-
-    if (lua_isstring(lua, -1))
-        *val = strdup(lua_tostring(lua, -1));
-
-    lua_pop(lua, 1);
-}
-
-static void readConfigCrtShader(Config* config, lua_State* lua)
-{
-    lua_getglobal(lua, "CRT_SHADER");
-
-    if(lua_type(lua, -1) == LUA_TTABLE)
-    {
-        readString(lua, "VERTEX", &config->data.shader.vertex);
-        readString(lua, "PIXEL", &config->data.shader.pixel);
-    }
-
-#if defined (EMSCRIPTEN)
-    // WebGL supports only version 100 shaders.
-    // Luckily, the format is nearly identical.
-    // This code detects the incompatible line(s) at
-    // the beginning of each shader and patches them
-    // in-place in memory.
-    char *s = (char *)config->data.shader.vertex;
-    if (strncmp("\t\t#version 110", s, 14) == 0) {
-        // replace the two tabs, with a "//" comment, disabling the #version tag.
-        s[0] = '/';
-        s[1] = '/';
-    }
-    s = (char *)config->data.shader.pixel;
-    if (strncmp("\t\t#version 110\n\t\t//precision highp float;", s, 41) == 0) {
-        // replace the two tabs, with a "//" comment, disabling the #version tag.
-        s[0] = '/';
-        s[1] = '/';
-        // replace the "//" comment with spaces, enabling the precision statement.
-        s[17] = ' ';
-        s[18] = ' ';
-    }
-#endif
-
-    lua_pop(lua, 1);        
-}
-
-#endif
-
 static void readCodeTheme(Config* config, lua_State* lua)
 {
     lua_getfield(lua, -1, "CODE");
@@ -208,9 +158,6 @@ static void readConfig(Config* config)
             if(config->data.uiScale <= 0)
                 config->data.uiScale = 1;
 
-#if defined(CRT_SHADER_SUPPORT)
-            readConfigCrtShader(config, lua);
-#endif
             readTheme(config, lua);
         }
 
@@ -351,12 +298,5 @@ void freeConfig(Config* config)
     tic_fs_saveroot(config->fs, OptionsDatPath, &config->data.options, sizeof config->data.options, true);
 
     free(config->cart);
-
-#if defined(CRT_SHADER_SUPPORT)
-
-    free((void*)config->data.shader.vertex);
-    free((void*)config->data.shader.pixel);
-#endif
-
     free(config);
 }
