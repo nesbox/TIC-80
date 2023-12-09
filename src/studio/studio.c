94e60778c39594d1a0e1be95d411c8b6deaadf12
Yue Wang <Wang-Yue@users.noreply.github.com>
2023-12-09 23:12:15 +0900

    remove unsequenced modification and access
    to improve readability and at the same time make the compiler happy.

--- src/studio/studio.c
+++ src/studio/studio.c
@@ -796,11 +796,23 @@ static inline float bounceOut(float x)
 {
     const float n1 = 7.5625;
     const float d1 = 2.75;
-
-    if (x < 1 / d1)         return n1 * x * x;
-    else if (x < 2 / d1)    return n1 * (x -= 1.5 / d1) * x + 0.75;
-    else if (x < 2.5 / d1)  return n1 * (x -= 2.25 / d1) * x + 0.9375;
-    else                    return n1 * (x -= 2.625 / d1) * x + 0.984375;
+        
+    if (x < 1 / d1) return n1 * x * x;
+    else if (x < 2 / d1)
+    {       
+        x -= 1.5 / d1;
+        return n1 * x * x + 0.75;
+    }   
+    else if (x < 2.5 / d1)
+    {   
+        x -= 2.25 / d1;
+        return n1 * x * x + 0.9375;
+    }   
+    else
+    {
+        x -= 2.625 / d1;
+        return n1 * x * x + 0.984375;
+    }
 }
 
 static inline float animEffect(AnimEffect effect, float x)
