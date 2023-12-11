4399fc087e94135a541c2e3ae60ecf9db00872c1
Yue Wang <Wang-Yue@users.noreply.github.com>
2023-12-11 16:26:43 +0900

    Fix memory leak of script in console.c

--- src/studio/screens/console.c
+++ src/studio/screens/console.c
@@ -3743,8 +3743,10 @@ static lua_State* netLuaInit(u8* buffer, s32 size)
         if(lua)
         {
             if(luaL_loadstring(lua, (char*)script) == LUA_OK && lua_pcall(lua, 0, LUA_MULTRET, 0) == LUA_OK)
+	    {
+                free(script);
                 return lua;
-
+	    }
             else lua_close(lua);
         }
 
