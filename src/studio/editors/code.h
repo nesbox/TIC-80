94ab63f69f79ca306ed40be94d16bc60a28cb29d
alice <aliceisjustplaying@gmail.com>
2024-04-12 18:32:27 +0100

    src/studio/editors/code.h

--- src/studio/editors/code.h
+++ src/studio/editors/code.h
@@ -140,5 +140,7 @@ struct Code
 
 void initCode(Code*, Studio* studio);
 void freeCode(Code*);
+void codeGetPos(Code*, s32* x, s32* y);
+void codeSetPos(Code*, s32 x, s32 y);
 
 void trimWhitespace(Code*);
