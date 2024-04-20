ed0ec2d2bdccdd3d3f5ef2d6b9b96d92c9743433
nesbox <grigoruk@gmail.com>
2024-04-20 20:48:18 +0200

    rpi build fix

--- cmake/quickjs.cmake
+++ cmake/quickjs.cmake
@@ -18,42 +18,48 @@ if(BUILD_WITH_JS)
         ${QUICKJS_DIR}/libregexp.c 
         ${QUICKJS_DIR}/libunicode.c
         ${QUICKJS_DIR}/cutils.c
+    )
+
+    add_library(quickjs STATIC ${QUICKJS_SRC})
+    target_compile_definitions(quickjs PUBLIC CONFIG_VERSION="${CONFIG_VERSION}")
+
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+        target_compile_definitions(quickjs PRIVATE DUMP_LEAKS)
+    endif()
+
+    if(BAREMETALPI OR N3DS)
+        target_compile_definitions(quickjs PRIVATE POOR_CLIB) 
+    endif()
+
+    if(LINUX)
+        target_compile_definitions(quickjs PUBLIC _GNU_SOURCE _POSIX_C_SOURCE=200112)
+        target_link_libraries(quickjs PUBLIC m dl pthread)
+    endif()
+    
+    set(JS_SRC
         ${CMAKE_SOURCE_DIR}/src/api/js.c
         ${CMAKE_SOURCE_DIR}/src/api/parse_note.c
     )
 
     if(BUILD_STATIC)
-        add_library(js STATIC ${QUICKJS_SRC})
+        add_library(js STATIC ${JS_SRC})
         target_compile_definitions(js PUBLIC TIC_BUILD_STATIC)
         target_link_libraries(js PRIVATE tic80core)
     else()
-        add_library(js SHARED ${QUICKJS_SRC})
+        add_library(js SHARED ${JS_SRC})
         set_target_properties(js PROPERTIES PREFIX "")
         if(MINGW)
             target_link_options(js PRIVATE -static)
         endif()
     endif()
 
-    target_compile_definitions(js PUBLIC CONFIG_VERSION="${CONFIG_VERSION}")
+    target_link_libraries(js PRIVATE quickjs)
     target_include_directories(js 
         PUBLIC ${QUICKJS_DIR}
         PRIVATE 
             ${CMAKE_SOURCE_DIR}/include
             ${CMAKE_SOURCE_DIR}/src
     )
-
-    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-        target_compile_definitions(js PRIVATE DUMP_LEAKS)
-    endif()
-
-    if(BAREMETALPI OR N3DS)
-        target_compile_definitions(js PRIVATE POOR_CLIB) 
-    endif()
-
-    if(LINUX)
-        target_compile_definitions(js PUBLIC _GNU_SOURCE _POSIX_C_SOURCE=200112)
-        target_link_libraries(js PUBLIC m dl pthread)
-    endif()
 
     target_compile_definitions(js INTERFACE TIC_BUILD_WITH_JS=1)
 
