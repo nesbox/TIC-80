c92885bc5cfd76941becf746be6ec1da43666105
Miguel <81558772+Miguel-hrvs@users.noreply.github.com>
2024-07-11 17:54:27 +0200

    fix f1 hotkey conflict

--- src/studio/editors/code.c
+++ src/studio/editors/code.c
@@ -2959,7 +2959,7 @@ static void processKeyboard(Code* code)
     bool usedKeybinding = true;
 
     // handle bookmarks
-    if(keyWasPressed(code->studio, tic_key_f1))
+    if(keyWasPressed(code->studio, tic_key_f10))
     {
         if(ctrl && shift)
         {
