2571cf25cfd77ff9cc55bb428faacb83148ca5e7
Des-Nerger <mixerator@rambler.ru>
2024-09-05 16:59:33 +1000

    Update to Zig 0.14.0-dev, run through `$ zig fmt`

--- templates/zig/build.zig
+++ templates/zig/build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) !void {
     const target = b.resolveTargetQuery(.{ .cpu_arch = .wasm32, .os_tag = .freestanding });
     const exe = b.addExecutable(.{
         .name = "cart",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
