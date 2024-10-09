c009e35c0f24a8a340bd746e7f122f48e96f8781
Miguel <81558772+Miguel-hrvs@users.noreply.github.com>
2024-10-09 14:10:10 +0200

    test

--- build/n3ds/elf_to_3dsx.sh
+++ build/n3ds/elf_to_3dsx.sh
@@ -2,4 +2,4 @@
 echo "[3DSX] Building metadata"
 ${DEVKITPRO}/tools/bin/smdhtool --create "TIC-80 tiny computer" "Fantasy computer for making, playing and sharing tiny games" "Nesbox" "n3ds/icon.png" tic80.smdh
 echo "[3DSX] Building binary"
-${DEVKITPRO}/tools/bin/3dsxtool bin/tic80_n3ds bin/tic80.3dsx --smdh=tic80.smdh --romfs=n3ds/romfs/
+${DEVKITPRO}/tools/bin/3dsxtool bin/tic80_n3ds.elf bin/tic80.3dsx --smdh=tic80.smdh --romfs=n3ds/romfs/
