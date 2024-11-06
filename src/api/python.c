fdf88640c27216e1e6a04030e44b01e6814b3820
PrimedErwin <1973609350@qq.com>
2024-11-06 10:47:10 +0800

    [dev]fixed mouse() alloc

--- src/api/python.c
+++ src/api/python.c
@@ -576,7 +576,8 @@ static bool py_mouse(int argc, py_Ref argv)
     tic_point pos = core->api.mouse(tic);
     const tic80_mouse* mouse = &core->memory.ram->input.mouse;
 
-    py_Ref res = (py_Ref)malloc(sizeof(py_Ref)*7);
+    //py_Ref res = (py_Ref)malloc(sizeof(py_Ref));
+    py_Ref res = py_r7();
     py_newtuple(res, 7);
     py_Ref tmp = py_retval();
     py_newint(tmp, pos.x);
