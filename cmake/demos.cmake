db270b211b39e495a5c63acea786fc6c461c3df5
Bryce Carson <bcars268@mtroyal.ca>
2024-11-07 00:28:20 -0700

    Check-in

--- cmake/demos.cmake
+++ cmake/demos.cmake
@@ -18,21 +18,13 @@ file(GLOB_RECURSE projects
 foreach(project ${projects})
   cmake_path(REPLACE_EXTENSION project LAST_ONLY "tic" OUTPUT_VARIABLE cartridge)
   cmake_path(GET cartridge FILENAME cartridge)
-  cmake_path(APPEND CMAKE_BINARY_DIR ${cartridge} OUTPUT_VARIABLE cartridge)
+  cmake_path(APPEND ${CMAKE_BINARY_DIR} ${cartridge} OUTPUT_VARIABLE cartridge)
   LIST(APPEND cartridges ${cartridge})
   message(DEBUG "${project}")
   message(DEBUG "${cartridge}")
 endforeach()
 
-add_custom_target(GenerateTicDatFiles
-  ALL
-  ## Generate .tic files in the CMAKE_BINARY_DIR directory using source files
-  ## from the demos/ source directory.
-  COMMAND prj2cart -i ${projects} -o ${CMAKE_BINARY_DIR}
-  ## Convert the .tic files in the CMAKE_BINARY_DIR to .tic.dat and place the
-  ## resulting file in the build/assets source directory.
-  COMMAND bin2txt -zi ${cartridges} -o ${buildAssetDir}
-  COMMENT "[DEMOS_AND_BUNNYS] Generating build assets (.tic.dat files)"
-  COMMAND_EXPAND_LISTS)
+# <<generate the tic.dat files>>
+<<generate the tic.dat files using the original tools>>
 
 target_sources(GenerateTicDatFiles PRIVATE ${PROJECT_PATHS})
