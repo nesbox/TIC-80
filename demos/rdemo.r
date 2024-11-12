7473a8530e7a894812fce33e6885f6d399ae4ce4
Bryce Carson <bcars268@mtroyal.ca>
2024-11-12 01:28:32 -0700

    Update rdemo.r

--- demos/rdemo.r
+++ demos/rdemo.r
@@ -10,57 +10,20 @@ t <- 0
 x <- 96
 y <- 24
 
-inc <- \(x) eval.parent(substitute(x <- x + 1))
-dec <- \(x) eval.parent(substitute(x <- x - 1))
+.X <- .External
 
-## The program eventually SEGFAULTs with the following message.
-##  *** caught segfault ***
-## address (nil), cause 'memory not mapped'
 `TIC-80` <- function() {
-  mapply(function(b, o) {
-    if (.External("t80.btn", b)) {
-      eval(as.expression(o), .GlobalEnv)
-    }
-  },
-  0:3,
-  list(quote(dec(y)), quote(inc(y)),
-       quote(dec(x)), quote(inc(x))));
-  .External("t80.cls", 13);
-  .External("t80.spr",
-            id = 1 + (t %% 60) / 30 * 2,
-            scale = 3,
-            x, y,
-            colorkey = 14,
-            w = 2, h = 2);
-  .External("t80.print", "HELLO WORLD!", 84, 84);
+  if (.X("t80.btn", 0)) y <- y - 1
+  if (.X("t80.btn", 1)) y <- y + 1
+  if (.X("t80.btn", 2)) x <- x - 1
+  if (.X("t80.btn", 3)) x <- x + 1
+  .X("t80.cls", 13);
+  .X("t80.spr",
+     id = 1+(t%%60)/30*2,
+     scale = 3,
+     x, y,
+     colorkey = 14,
+     w = 2, h = 2);
+  .X("t80.print", "HELLO WORLD!", 84, 84);
   inc(t);
 }
-
-## <TILES>
-## 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
-## 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
-## 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
-## 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
-## 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
-## 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
-## 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
-## 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
-## </TILES>
-
-## <WAVES>
-## 000:00000000ffffffff00000000ffffffff
-## 001:0123456789abcdeffedcba9876543210
-## 002:0123456789abcdef0123456789abcdef
-## </WAVES>
-
-## <SFX>
-## 000:000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000304000000000
-## </SFX>
-
-## <PALETTE>
-## 000:1a1c2c5d275db13e53ef7d57ffcd75a7f07038b76425717929366f3b5dc941a6f673eff7f4f4f494b0c2566c86333c57
-## </PALETTE>
-
-## <TRACKS>
-## 000:100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-## </TRACKS>
