a5d3ea9c4543a76a15bd5df806124416115af415
Bryce Carson <bcars268@mtroyal.ca>
2024-11-11 23:30:45 -0700

    Reorganize lines in Windows BATCH script

--- assets.bat
+++ assets.bat
@@ -1,29 +1,31 @@
 build\bin\prj2cart config.js build\config.tic
-build\bin\prj2cart demos\luademo.lua build\luademo.tic
+
 build\bin\prj2cart demos\benchmark.lua build\benchmark.tic
 build\bin\prj2cart demos\bpp.lua build\bpp.tic
 build\bin\prj2cart demos\car.lua build\car.tic
-build\bin\prj2cart demos\fenneldemo.fnl build\fenneldemo.tic
 build\bin\prj2cart demos\fire.lua build\fire.tic
 build\bin\prj2cart demos\font.lua build\font.tic
-build\bin\prj2cart demos\janetdemo.janet build\janetdemo.tic
-build\bin\prj2cart demos\jsdemo.js build\jsdemo.tic
-build\bin\prj2cart demos\luademo.lua build\luademo.tic
-build\bin\prj2cart demos\moondemo.moon build\moondemo.tic
 build\bin\prj2cart demos\music.lua build\music.tic
 build\bin\prj2cart demos\p3d.lua build\p3d.tic
 build\bin\prj2cart demos\palette.lua build\palette.tic
-build\bin\prj2cart demos\pythondemo.py build\pythondemo.tic
 build\bin\prj2cart demos\quest.lua build\quest.tic
+build\bin\prj2cart demos\sfx.lua build\sfx.tic
+build\bin\prj2cart demos\tetris.lua build\tetris.tic
+
+build\bin\prj2cart demos\fenneldemo.fnl build\fenneldemo.tic
+build\bin\prj2cart demos\janetdemo.janet build\janetdemo.tic
+build\bin\prj2cart demos\jsdemo.js build\jsdemo.tic
+build\bin\prj2cart demos\luademo.lua build\luademo.tic
+build\bin\prj2cart demos\luademo.lua build\luademo.tic
+build\bin\prj2cart demos\moondemo.moon build\moondemo.tic
+build\bin\prj2cart demos\pythondemo.py build\pythondemo.tic
 build\bin\prj2cart demos\rubydemo.rb build\rubydemo.tic
 build\bin\prj2cart demos\schemedemo.scm build\schemedemo.tic
-build\bin\prj2cart demos\sfx.lua build\sfx.tic
 build\bin\prj2cart demos\squirreldemo.nut build\squirreldemo.tic
-build\bin\prj2cart demos\tetris.lua build\tetris.tic
 build\bin\prj2cart demos\wrendemo.wren build\wrendemo.tic
 
-build\bin\wasmp2cart demos\wasm\wasmdemo.wasmp build\wasmdemo.tic --binary demos\wasm\wasmdemo.wasm
 build\bin\wasmp2cart demos\bunny\wasmmark\wasmmark.wasmp build\wasmmark.tic --binary demos\bunny\wasmmark\wasmmark.wasm
+build\bin\wasmp2cart demos\wasm\wasmdemo.wasmp build\wasmdemo.tic --binary demos\wasm\wasmdemo.wasm
 
 build\bin\prj2cart demos\bunny\janetmark.janet build\janetmark.tic
 build\bin\prj2cart demos\bunny\jsmark.js build\jsmark.tic
@@ -35,29 +37,32 @@ build\bin\prj2cart demos\bunny\schememark.scm build\schememark.tic
 build\bin\prj2cart demos\bunny\squirrelmark.nut build\squirrelmark.tic
 build\bin\prj2cart demos\bunny\wrenmark.wren build\wrenmark.tic
 
-build\bin\bin2txt build\config.tic build\assets\config.tic.dat -z
-build\bin\bin2txt build\luademo.tic build\assets\luademo.tic.dat -z
-build\bin\bin2txt build\benchmark.tic build\assets\benchmark.tic.dat -z
 build\bin\bin2txt build\bpp.tic build\assets\bpp.tic.dat -z
 build\bin\bin2txt build\car.tic build\assets\car.tic.dat -z
-build\bin\bin2txt build\fenneldemo.tic build\assets\fenneldemo.tic.dat -z
+build\bin\bin2txt build\config.tic build\assets\config.tic.dat -z
 build\bin\bin2txt build\fire.tic build\assets\fire.tic.dat -z
 build\bin\bin2txt build\font.tic build\assets\font.tic.dat -z
-build\bin\bin2txt build\janetdemo.tic build\assets\janetdemo.tic.dat -z
-build\bin\bin2txt build\jsdemo.tic build\assets\jsdemo.tic.dat -z
-build\bin\bin2txt build\luademo.tic build\assets\luademo.tic.dat -z
-build\bin\bin2txt build\moondemo.tic build\assets\moondemo.tic.dat -z
 build\bin\bin2txt build\music.tic build\assets\music.tic.dat -z
 build\bin\bin2txt build\p3d.tic build\assets\p3d.tic.dat -z
 build\bin\bin2txt build\palette.tic build\assets\palette.tic.dat -z
-build\bin\bin2txt build\pythondemo.tic build\assets\pythondemo.tic.dat -z
 build\bin\bin2txt build\quest.tic build\assets\quest.tic.dat -z
+build\bin\bin2txt build\sfx.tic build\assets\sfx.tic.dat -z
+build\bin\bin2txt build\tetris.tic build\assets\tetris.tic.dat -z
+
+build\bin\bin2txt build\fenneldemo.tic build\assets\fenneldemo.tic.dat -z
+build\bin\bin2txt build\janetdemo.tic build\assets\janetdemo.tic.dat -z
+build\bin\bin2txt build\jsdemo.tic build\assets\jsdemo.tic.dat -z
+build\bin\bin2txt build\luademo.tic build\assets\luademo.tic.dat -z
+build\bin\bin2txt build\luademo.tic build\assets\luademo.tic.dat -z
+build\bin\bin2txt build\moondemo.tic build\assets\moondemo.tic.dat -z
+build\bin\bin2txt build\pythondemo.tic build\assets\pythondemo.tic.dat -z
 build\bin\bin2txt build\rubydemo.tic build\assets\rubydemo.tic.dat -z
 build\bin\bin2txt build\schemedemo.tic build\assets\schemedemo.tic.dat -z
-build\bin\bin2txt build\sfx.tic build\assets\sfx.tic.dat -z
 build\bin\bin2txt build\squirreldemo.tic build\assets\squirreldemo.tic.dat -z
-build\bin\bin2txt build\tetris.tic build\assets\tetris.tic.dat -z
+build\bin\bin2txt build\wasmdemo.tic build\assets\wasmdemo.tic.dat -z
 build\bin\bin2txt build\wrendemo.tic build\assets\wrendemo.tic.dat -z
+
+build\bin\bin2txt build\benchmark.tic build\assets\benchmark.tic.dat -z
 build\bin\bin2txt build\janetmark.tic build\assets\janetmark.tic.dat -z
 build\bin\bin2txt build\jsmark.tic build\assets\jsmark.tic.dat -z
 build\bin\bin2txt build\luamark.tic build\assets\luamark.tic.dat -z
@@ -68,6 +73,5 @@ build\bin\bin2txt build\schememark.tic build\assets\schememark.tic.dat -z
 build\bin\bin2txt build\squirrelmark.tic build\assets\squirrelmark.tic.dat -z
 build\bin\bin2txt build\wasmmark.tic build\assets\wasmmark.tic.dat -z
 build\bin\bin2txt build\wrenmark.tic build\assets\wrenmark.tic.dat -z
-build\bin\bin2txt build\wasmdemo.tic build\assets\wasmdemo.tic.dat -z
 
 build\bin\bin2txt build\cart.png build\assets\cart.png.dat
