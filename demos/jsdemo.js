b75726373b7b074c399bec4dbf486756508c53d7
Elias <92587199+eliaswolfy@users.noreply.github.com>
2024-11-21 00:10:09 -0400

    Update jsdemo.js

--- demos/jsdemo.js
+++ demos/jsdemo.js
@@ -6,21 +6,21 @@
 // version: 0.1
 // script:  js
 
-var t=0
-var x=96
-var y=24
+let t=0;
+let x=96;
+let y=24;
 
 function TIC()
 {
-	if(btn(0))y--
-	if(btn(1))y++
-	if(btn(2))x--
-	if(btn(3))x++
+	if(btn(0))y--;
+	if(btn(1))y++;
+	if(btn(2))x--;
+	if(btn(3))x++;
 
-	cls(13)
-	spr(1+((t%60)/30|0)*2,x,y,14,3,0,0,2,2)
-	print("HELLO WORLD!",84,84)
-	t++
+	cls(13);
+	spr(1+((t%60)/30|0)*2,x,y,14,3,0,0,2,2);
+	print("HELLO WORLD!",84,84);
+	t++;
 }
 
 // <TILES>
