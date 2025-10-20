b3feccf7704fa32d33d458d722d1eeff886c54ab
Dania Rifki <atomicnumberphi@gmail.com>
2025-10-21 00:08:09 +0700

    Add YueScript as an additional scripting language (#2743)
    * Publish Code
    
    * Fixed YueScript build.
    
    * Disabled macro feature which crashes runtime.
    
    * Update YueScript submodule
    
    * Try to fix the "function TIC()..." isn't found bug, no luck so far...
    
    * Fix Demo and Mark
    
    * Format yue.cpp
    
    * Make it work in C99, let's hope this compiles in Win XP et cetera
    
    * Fix Compilation Issues
    
    * Try fixing rpi-baremetal(4) and nintendo-3ds builds
    
    * Try fixing linking
    
    * Reenable Exceptions
    
    * Override YueParser
    
    * Try forcing newer glibc?
    
    * Debugging Build Step
    
    * Update YueScript version
    
    * Simplify build
    
    * Add yue.cpp
    
    * Whoops
    
    * Update YueScript
    
    * Update circle.patch to try to fix the build error
    
    * Try removing -lstdc++fs
    
    * Modify Patch
    
    * Revert Update circle.patch
    
    * ???
    
    * Forgot to git Apply
    
    * Accidentally put it in macOS instead of N3DS
    
    * Remove platform specific configurations for now
    
    * Remove thread_local for N3DS Build
    
    * Use a more reliable way to disable thread_local on N3DS
    
    * Forgot to pass in initial-exec
    
    * Remove build/yuescript-makefile-remove-stdc++fs.patch
    
    * Include libyuescript in the baremetal Makefile
    
    ---------
    
    Co-authored-by: Li Jin <dragon-fly@qq.com>

--- cmake/core.cmake
+++ cmake/core.cmake
@@ -51,6 +51,7 @@ target_include_directories(tic80core
     PRIVATE
         ${THIRDPARTY_DIR}/moonscript
         ${THIRDPARTY_DIR}/fennel
+        ${THIRDPARTY_DIR}/yuescript
         ${POCKETPY_DIR}/src
     PUBLIC
         ${CMAKE_SOURCE_DIR}/include
@@ -70,6 +71,10 @@ if(BUILD_STATIC)
 
     if(BUILD_WITH_MOON)
         target_link_libraries(tic80core PRIVATE moon)
+    endif()
+
+    if(BUILD_WITH_YUE)
+        target_link_libraries(tic80core PRIVATE yuescript)
     endif()
 
     if(BUILD_WITH_FENNEL)
---
+++ cmake/yue.cmake
@@ -0,0 +1,70 @@
+################################
+# YUESCRIPT
+################################
+
+option(BUILD_WITH_YUE "Yue Enabled" ${BUILD_WITH_ALL})
+message("BUILD_WITH_YUE: ${BUILD_WITH_YUE}")
+
+if(BUILD_WITH_YUE)
+    set(YUESCRIPT_DIR ${THIRDPARTY_DIR}/yuescript)
+    set(YUESCRIPT_SRC
+        ${YUESCRIPT_DIR}/src/yuescript/ast.cpp
+        ${YUESCRIPT_DIR}/src/yuescript/parser.cpp
+        ${YUESCRIPT_DIR}/src/yuescript/yue_ast.cpp
+        ${YUESCRIPT_DIR}/src/yuescript/yue_parser.cpp
+        ${YUESCRIPT_DIR}/src/yuescript/yue_compiler.cpp
+        ${YUESCRIPT_DIR}/src/yuescript/yuescript.cpp
+    )
+
+    list(APPEND YUESCRIPT_SRC ${CMAKE_SOURCE_DIR}/src/api/yue.cpp)
+    list(APPEND YUESCRIPT_SRC ${CMAKE_SOURCE_DIR}/src/api/parse_note.c)
+
+    add_library(yuescript ${TIC_RUNTIME} ${YUESCRIPT_SRC})
+
+    if(NOT BUILD_STATIC)
+        set_target_properties(yuescript PROPERTIES PREFIX "")
+    else()
+        target_compile_definitions(yuescript INTERFACE TIC_BUILD_WITH_YUE)
+    endif()
+
+    target_link_libraries(yuescript
+        PRIVATE
+        runtime
+        luaapi
+    )
+
+    set_target_properties(yuescript PROPERTIES
+        LINKER_LANGUAGE CXX
+        CXX_STANDARD 17
+        CXX_STANDARD_REQUIRED ON
+    )
+
+    target_include_directories(yuescript
+        PRIVATE
+        ${YUESCRIPT_DIR}/src
+        ${YUESCRIPT_DIR}/src/yuescript
+        ${CMAKE_SOURCE_DIR}/include
+        ${CMAKE_SOURCE_DIR}/src
+        ${CMAKE_SOURCE_DIR}/src/api
+    )
+
+    target_compile_definitions(yuescript PRIVATE 
+        YUE_NO_MACRO 
+        $<$<BOOL:${MSVC}>:_SCL_SECURE_NO_WARNINGS>
+    )
+
+    if(NINTENDO_3DS)
+        target_compile_options(yuescript PRIVATE -ftls-model=initial-exec)
+    endif()
+
+    if(MSVC)
+        target_compile_definitions(yuescript PRIVATE _SCL_SECURE_NO_WARNINGS)
+    else()
+        target_compile_options(yuescript PRIVATE -Wall -Wno-long-long -fPIC -O3)
+    endif()
+
+    if(APPLE)
+        target_compile_options(yuescript PRIVATE -Wno-deprecated-declarations)
+    endif()
+
+endif()
