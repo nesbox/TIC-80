8f7f36d2db99748bef8c65ee48657937ce4764cc
Dania Rifki <atomicnumberphi@gmail.com>
2026-02-07 18:10:19 +0700

    Update vendor/zip from v0.1.19 to v0.3.6 (#2883)
    * Updates verdor/zip from v0.1.19 to v0.3.5
    
    no need for the -DCMAKE_POLICY_VERSION_MINIMUM=3.5 flag anymore.
    
    * uses zip v0.2.6 instead of v.3.5
    
    due to:
    zip/src/miniz.h:8503:30: error: comparison is always false due to
    limited range of data type [-Werror=type-limits]
    8503 |     if (((mz_uint64)buf_size > 0xFFFFFFFF) || (uncomp_size >
    0xFFFFFFFF)) {
    
    * tries zip v0.3.5 again
    
    but with the -Wno-type-limits flag
    
    * Update vendor/zip to 0.3.6
    
    * Add -Wno-stringop-truncation to zip compile options
    
    * Refactor compiler option checks for zip to only apply -Wno-stringop-truncation for GCC
    
    * Fix for  Baremetal GCC treating string truncation warnings as errors
    
    * Fix support for bare-metal environment by defining lstat as stat
    
    * Format comment
    
    ---------
    
    Co-authored-by: imsys <911254+imsys@users.noreply.github.com>

--- cmake/zip.cmake
+++ cmake/zip.cmake
@@ -20,3 +20,22 @@ endif()
 
 set(CMAKE_DISABLE_TESTING ON CACHE BOOL "" FORCE)
 add_subdirectory(${THIRDPARTY_DIR}/zip)
+
+if(CMAKE_C_COMPILER_ID MATCHES "Clang|GNU")
+    target_compile_options(zip PRIVATE -Wno-type-limits)
+#  -Wno-type-limits due to:
+#     zip/src/miniz.h:8503:30: error: comparison is always false due to limited range of data type [-Werror=type-limits]
+#  8503 |     if (((mz_uint64)buf_size > 0xFFFFFFFF) || (uncomp_size > 0xFFFFFFFF)) {
+endif()
+if(CMAKE_C_COMPILER_ID STREQUAL "GNU" OR BAREMETALPI)
+    target_compile_options(zip PRIVATE -Wno-stringop-truncation)
+#  -Wno-stringop-truncation due to:
+#     /home/runner/work/TIC-80/TIC-80/vendor/zip/src/zip.c: In function 'zip_archive_extract':
+#     /home/runner/work/TIC-80/TIC-80/vendor/zip/src/zip.c:191:3: error: 'strncpy' output may be truncated copying between 0 and 512 bytes from a string of length 512 [-Werror=stringop-truncation]
+#  191 |   strncpy(npath, path, len);
+endif()
+if(BAREMETALPI)
+    # lstat is not available in circle-stdlib (bare-metal environment)
+    # Use stat instead, symlink handling isn't relevant on bare-metal anyway
+    target_compile_definitions(zip PRIVATE lstat=stat)
+endif()
