86cc54555f4d0a3ce9a27191cba15c29b2bb58fa
imsys <911254+imsys@users.noreply.github.com>
2026-03-09 08:21:00 -0300

    Implement monolithic static libretro build
    Collect sources, includes, and defines for a monolithic library build.

--- cmake/libretro.cmake
+++ cmake/libretro.cmake
@@ -9,9 +9,77 @@ if(BUILD_LIBRETRO)
     )
 
     if (LIBRETRO_STATIC)
-        add_library(tic80_libretro STATIC
-            ${LIBRETRO_SRC}
+        # Collect sources from all dependencies to build a monolithic library.
+        # This ensures that consoles and Emscripten get a single, complete archive.
+        set(TIC80_MONOLITHIC_SRCS ${LIBRETRO_SRC})
+        
+        # List of all potential core component targets to collect sources from
+        set(TIC80_CORE_OBJECT_DEPS 
+            tic80core luaapi lua lpeg moon yue fennel wren squirrel pocketpy 
+            quickjs mruby wasm scheme janet zip zlib png blipbuf giflib 
+            wave argparse naett
         )
+
+        set(TIC80_MONOLITHIC_INCLUDES "")
+        set(TIC80_MONOLITHIC_DEFINES "")
+
+        foreach(target ${TIC80_CORE_OBJECT_DEPS})
+            if(TARGET ${target})
+                # Collect sources
+                get_target_property(target_sources ${target} SOURCES)
+                get_target_property(target_dir ${target} SOURCE_DIR)
+                if(target_sources)
+                    foreach(src ${target_sources})
+                        if(NOT IS_ABSOLUTE "${src}")
+                            set(src "${target_dir}/${src}")
+                        endif()
+                        if(EXISTS "${src}")
+                            list(APPEND TIC80_MONOLITHIC_SRCS "${src}")
+                        endif()
+                    endforeach()
+                endif()
+
+                # Collect ALL include directories (Private and Interface)
+                get_target_property(includes ${target} INCLUDE_DIRECTORIES)
+                if(includes)
+                    list(APPEND TIC80_MONOLITHIC_INCLUDES ${includes})
+                endif()
+                get_target_property(iface_includes ${target} INTERFACE_INCLUDE_DIRECTORIES)
+                if(iface_includes)
+                    list(APPEND TIC80_MONOLITHIC_INCLUDES ${iface_includes})
+                endif()
+
+                # Collect ALL compile definitions (Private and Interface and Directory)
+                get_target_property(defines ${target} COMPILE_DEFINITIONS)
+                if(defines)
+                    list(APPEND TIC80_MONOLITHIC_DEFINES ${defines})
+                endif()
+                get_target_property(iface_defines ${target} INTERFACE_COMPILE_DEFINITIONS)
+                if(iface_defines)
+                    list(APPEND TIC80_MONOLITHIC_DEFINES ${iface_defines})
+                endif()
+                get_directory_property(dir_defines DIRECTORY ${target_dir} COMPILE_DEFINITIONS)
+                if(dir_defines)
+                    list(APPEND TIC80_MONOLITHIC_DEFINES ${dir_defines})
+                endif()
+            endif()
+        endforeach()
+
+        # Remove duplicates from includes/defines to keep command lines manageable
+        if(TIC80_MONOLITHIC_INCLUDES)
+            list(REMOVE_DUPLICATES TIC80_MONOLITHIC_INCLUDES)
+        endif()
+        if(TIC80_MONOLITHIC_DEFINES)
+            list(REMOVE_DUPLICATES TIC80_MONOLITHIC_DEFINES)
+        endif()
+
+        add_library(tic80_libretro STATIC
+            ${TIC80_MONOLITHIC_SRCS}
+        )
+
+        target_include_directories(tic80_libretro PRIVATE ${TIC80_MONOLITHIC_INCLUDES})
+        target_compile_definitions(tic80_libretro PRIVATE ${TIC80_MONOLITHIC_DEFINES})
+
         if(EMSCRIPTEN)
             set(LIBRETRO_EXTENSION "bc")
         else()
@@ -46,6 +114,8 @@ if(BUILD_LIBRETRO)
     target_compile_definitions(tic80_libretro PRIVATE
         __LIBRETRO__=TRUE
     )
-    target_link_libraries(tic80_libretro tic80core)
+    if(NOT LIBRETRO_STATIC)
+        target_link_libraries(tic80_libretro tic80core)
+    endif()
     set_target_properties(tic80_libretro PROPERTIES PREFIX "")
 endif()
