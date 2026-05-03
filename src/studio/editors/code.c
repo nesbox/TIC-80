bd790655c93cd45100da721b4de218c7ecac4ee2
borb <borbware@gmail.com>
2026-05-03 14:34:19 +0300

    remove MAX_CODE changes

--- src/studio/editors/code.c
+++ src/studio/editors/code.c
@@ -35,9 +35,9 @@
 #define SIDEBAR_WIDTH (12 * TIC_FONT_WIDTH)
 
 #if defined(TIC80_PRO)
-#   define MAX_CODE sizeof(tic_code) * 8
+#   define MAX_CODE sizeof(tic_code)
 #else
-#   define MAX_CODE TIC_BANK_SIZE * 8 * 8
+#   define MAX_CODE TIC_BANK_SIZE
 #endif
 
 #define noop (void)0
